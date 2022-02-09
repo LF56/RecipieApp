803c894ef3adbce099361a26cbe1cb64fad156d6
htam <hdfair@gmail.com>
2022-02-08 19:22:32 -0500

    update api key

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
 var ingredientInputEl = document.querySelector("input");
 var buttonEl = document.querySelector("#btn-search")
 // key entered by heather 2/8/22
-var APIKey = "d4e0d777f34d41bf918edcac7b1fe3e2";
+var APIKey = "81c6c46f0a9344f2951ce2eff8cef15e";
 var recipeContainer = $("#recipe-container");
 
 //recipes search function
